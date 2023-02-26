9e55c184fd7dbef625ad08b486cf3bbe48131d8e
Михаил <misga.yats@gmail.com>
2023-02-26 12:52:10 +0100

    tests done, 1 homework is done

---
+++ tests/tests.py
@@ -0,0 +1,32 @@
+from main.classes import Node, Stack
+import unittest
+
+
+class TestNode(unittest.TestCase):
+    def setUp(self):
+        self.node = Node(69)
+
+    def test_node_init(self):
+        self.assertEqual(self.node.data, 69)
+        self.assertEqual(self.node.next_node, None)
+
+    def test_node_next_node(self):
+        node2 = Node(1, self.node)
+        self.assertEqual(node2.next_node, self.node)
+
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_stack_init(self):
+        self.assertEqual(self.stack.top, None)
+
+    def test_stack_push(self):
+        self.stack.push('data1')
+        self.stack.push('data2')
+        self.stack.push('data3')
+        self.assertEqual(self.stack.top.data, 'data3')
+        self.assertEqual(self.stack.top.next_node.data, 'data2')
+        self.assertEqual(self.stack.top.next_node.next_node.data, 'data1')
+        self.assertEqual(self.stack.top.next_node.next_node.next_node, None)
