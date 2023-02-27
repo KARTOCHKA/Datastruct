a066e1bd2eacc13d7a072453df500e72b48cee73
Михаил <misga.yats@gmail.com>
2023-02-27 13:42:33 +0100

    class updated, 2 homework done

--- main/classes.py
+++ main/classes.py
@@ -9,7 +9,15 @@ class Stack:
         self.top = None
 
     """Добавление в стек"""
+
     def push(self, data):
         node = Node(data)
         node.next_node = self.top
         self.top = node
+
+    """Удаляет из стека верхний элемент и возвращает данные удаленного экземпляра класса Node"""
+
+    def pop(self):
+        del_data = self.top
+        self.top = self.top.next_node
+        return del_data.data
