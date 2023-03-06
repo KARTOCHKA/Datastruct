f22456ba42db0d6a80872124202f03f12e4b1492
Михаил <misga.yats@gmail.com>
2023-03-06 12:55:35 +0100

    tests added

--- tests/tests.py
+++ tests/tests.py
@@ -24,26 +24,26 @@ class TestStack(unittest.TestCase):
         self.assertEqual(self.stack.top, None)
 
     def test_stack_push(self):
-        self.stack.push('data1')
-        self.stack.push('data2')
-        self.stack.push('data3')
-        self.assertEqual(self.stack.top.data, 'data3')
-        self.assertEqual(self.stack.top.next_node.data, 'data2')
-        self.assertEqual(self.stack.top.next_node.next_node.data, 'data1')
+        self.stack.push('test_data1')
+        self.stack.push('test_data2')
+        self.stack.push('test_data3')
+        self.assertEqual(self.stack.top.data, 'test_data3')
+        self.assertEqual(self.stack.top.next_node.data, 'test_data2')
+        self.assertEqual(self.stack.top.next_node.next_node.data, 'test_data1')
         self.assertEqual(self.stack.top.next_node.next_node.next_node, None)
 
     def test_stack_pop_add_data_one(self):
-        self.stack.push('data1')
+        self.stack.push('test_data1')
         data1 = self.stack.pop()
-        self.assertEqual(data1, 'data1')
+        self.assertEqual(data1, 'test_data1')
         self.assertEqual(self.stack.top, None)
 
     def test_stack_pop_add_data_two(self):
-        self.stack.push('data1')
-        self.stack.push('data2')
+        self.stack.push('test_data1')
+        self.stack.push('test_data2')
         data2 = self.stack.pop()
-        self.assertEqual(data2, 'data2')
-        self.assertEqual(self.stack.top.data, 'data1')
+        self.assertEqual(data2, 'test_data2')
+        self.assertEqual(self.stack.top.data, 'test_data1')
 
 
 class TestQueue(unittest.TestCase):
@@ -55,18 +55,18 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(self.queue.tail, None)
 
     def test_enqueue(self):
-        self.queue.enqueue('data1')
-        self.queue.enqueue('data2')
-        self.queue.enqueue('data3')
-        self.assertEqual(self.queue.head.data, 'data1')
-        self.assertEqual(self.queue.tail.data, 'data3')
+        self.queue.enqueue('test_data1')
+        self.queue.enqueue('test_data2')
+        self.queue.enqueue('test_data3')
+        self.assertEqual(self.queue.head.data, 'test_data1')
+        self.assertEqual(self.queue.tail.data, 'test_data3')
         self.assertEqual(self.queue.tail.next_node, None)
 
     def test_dequeue(self):
-        self.queue.enqueue('data1')
-        self.queue.enqueue('data2')
-        self.queue.enqueue('data3')
-        self.assertEqual(self.queue.dequeue(), 'data1')
-        self.assertEqual(self.queue.dequeue(), 'data2')
-        self.assertEqual(self.queue.dequeue(), 'data3')
+        self.queue.enqueue('test_data1')
+        self.queue.enqueue('test_data2')
+        self.queue.enqueue('test_data3')
+        self.assertEqual(self.queue.dequeue(), 'test_data1')
+        self.assertEqual(self.queue.dequeue(), 'test_data2')
+        self.assertEqual(self.queue.dequeue(), 'test_data3')
         self.assertEqual(self.queue.dequeue(), None)
